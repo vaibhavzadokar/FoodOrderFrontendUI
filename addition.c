76c7ac09b8eecc630a3b1420fcfc255f8b43d2ea
vaibhavzadokar <vaibhavzadokar12@gmail.com>
2022-07-14 17:26:49 +0530

    changes

---
+++ addition.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int num1, num2, num3;
+    printf("\n  Enter the two values for addition");
+    scanf("\n%d %d",&num1, &num2);
+    sum = num1 + num2;
+    printf("\n Addition is =%d",sum);
+    return 0;
+
+}
